7852a507f1e3677305b7af0cf6a17a5b777cfca4
Benaya Oktavianus <53290795+benayaokta@users.noreply.github.com>
2021-07-21 15:36:59 +0700

    new custom uiview folder

---
+++ Portfolio One/View/CustomUIView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomUIView.swift
+//  Portfolio One
+//
+//  Created by Benaya Oktavianus on 21/07/21.
+//
+
+import UIKit
+
+class CustomUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
